52deef8fd9cc58c2a2b3e58a98ca68a9cc5959e6
annalhq <155549764+annalhq@users.noreply.github.com>
2025-03-25 00:02:22 +0530

    feat: update django apps

---
+++ project/app3/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, '../templates/index.html')
+
+def service(request):
+    return render(request, '../templates/service.html')
